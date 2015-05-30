c173dc03a9cb01bfe925b1c221d6e5ad9d0570cc
Jack <xjlin0@gmail.com>
2015-05-30 12:06:28 -0700

    installed geokit > 2.1.0

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,4 +3,9 @@ class Report < ActiveRecord::Base
   has_many :report_tags
   has_many :tags, through: :report_tags
   has_many :comments
+  acts_as_mappable default_units: :miles,
+                   default_formula: :sphere,
+                   distance_field_name: :distance,
+                   lat_column_name: :lat,
+                   lng_column_name: :lng
 end
