6bf28d3ea2a454d1711d33c9a8f375346d9350d8
Jessica Grebenschikov <jessica.greben1@gmail.com>
2015-05-31 20:42:03 -0700

    wip- add CORS, working on mapquery route

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,27 @@
 class ReportsController < ApplicationController
+  # before_filter :cors_preflight_check
+  # after_filter :cors_set_access_control_headers
+
+  # def cors_set_access_control_headers
+  #   headers['Access-Control-Request-Method'] = '*'
+  #   headers['Access-Control-Allow-Origin'] = '*'
+  #   headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+  #   headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+  #   headers['Access-Control-Max-Age'] = '1728000'
+  # end
+
+  # def cors_preflight_check
+  #   if request.method == 'OPTIONS'
+  #     headers['Access-Control-Allow-Origin'] = '*'
+  #     headers['Access-Control-Request-Method'] = '*'
+
+  #     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+  #     headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+  #     headers['Access-Control-Max-Age'] = '1728000'
+
+  #     render :text => '', :content_type => 'text/plain'
+  #   end
+    
   def create
     @report = Report.new report_params
     @report.user_id = @current_user["id"]
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  match 'api/v1/reports', to: 'reports#create', via: [:post]
+  match 'api/v1/reports', to: 'reports#create', via: [:post, :options]
   get 'reports/mapquery'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
