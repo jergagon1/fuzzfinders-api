ce9a1aa1875f0052329f83eb1ff774894633fcb6
Jeremy Gagon <jergagon1@gmail.com>
2015-07-19 17:16:29 -0700

    Update schema for remarks table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150618231959) do
+ActiveRecord::Schema.define(version: 20150719214935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,17 @@ ActiveRecord::Schema.define(version: 20150618231959) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "remarks", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "article_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "remarks", ["article_id"], name: "index_remarks_on_article_id", using: :btree
+  add_index "remarks", ["user_id"], name: "index_remarks_on_user_id", using: :btree
 
   create_table "report_tags", force: :cascade do |t|
     t.integer  "tag_id"
