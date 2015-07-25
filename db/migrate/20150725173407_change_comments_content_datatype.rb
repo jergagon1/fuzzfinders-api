da492ac58e73ffca81f094222bd162142113f5df
Jeremy Gagon <jergagon1@gmail.com>
2015-07-25 10:38:39 -0700

    Change comments content datatype to text

---
+++ db/migrate/20150725173407_change_comments_content_datatype.rb
@@ -0,0 +1,5 @@
+class ChangeCommentsContentDatatype < ActiveRecord::Migration
+  def change
+    change_column :comments, :content, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150724204327) do
+ActiveRecord::Schema.define(version: 20150725173407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20150724204327) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "content"
+    t.text     "content"
     t.integer  "report_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
