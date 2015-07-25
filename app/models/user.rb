e27d5c665a0f2882936122e701f92c3c9bb33591
Jeremy Gagon <jergagon1@gmail.com>
2015-07-25 11:10:26 -0700

    Add user foreign key to reports

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   validates :username, :email, :password_hash, presence: true
   validates :username, :email, uniqueness: true
+  has_many :reports
 
-  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150725174214) do
+ActiveRecord::Schema.define(version: 20150725180021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,4 +86,5 @@ ActiveRecord::Schema.define(version: 20150725174214) do
     t.datetime "updated_at",                    null: false
   end
 
+  add_foreign_key "reports", "users"
 end
