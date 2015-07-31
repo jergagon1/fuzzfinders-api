92872154bbb47ab07bcfcc7a194273f8e3f6ab2e
Jeremy Gagon <jergagon1@gmail.com>
2015-07-31 09:32:21 -0700

    Handle pet color in dedicated field. Not tag. Add color field to reports table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150725232524) do
+ActiveRecord::Schema.define(version: 20150731162000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20150725232524) do
     t.float    "distance"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "color"
   end
 
   create_table "tags", force: :cascade do |t|
