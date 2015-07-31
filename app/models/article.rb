ce23ac9c63fda606a7bd43193e9c9b32221d7e42
Jeremy Gagon <jergagon1@gmail.com>
2015-07-31 16:56:21 -0700

    Progress on blog articles and tagging

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,15 @@ class Article < ActiveRecord::Base
   has_many :remarks
   has_many :article_tags
   has_many :tags, through: :article_tags
+
+  # article tagging getter, setter
+  def all_tags=(names)
+    self.tags = names.split(",").map do |name|
+      Tag.where(name: name.strip).first_or_create!
+    end
+  end
+
+  def all_tags
+    self.tags.map(&:name).join(", ")
+  end
 end
