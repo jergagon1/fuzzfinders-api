c5799f7faa0a824097158e3bc6219b8fb63a7565
Ryan Loughlin <rloughlin@castlighthealth.com>
2015-08-01 09:41:57 -0700

    update seed file to match schema. currently creates 1 user with 2 reports and 1 comment on the first report.  also ran bundle update so Gemfile.lock has changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,43 +1,43 @@
-# User.create(
-#   username: "testuser",
-#   email: "testuser@example.com",
-#   password_hash: "password",
-#   zipcode: 12345,
-#   wags: 0,
-#   reports: [
-    Report.create(
-      user_id: 1,
-      animal_type: "dog",
-      lat: 12.34567890,
-      lng: 9.87654321,
-      report_type: "found",
-      notes: "I found this dog",
-      img_url: "http://www.example.com/lost_dog.jpg",
-      size: "small",
-      distance: 10.53,
-      tags: [Tag.create(content: "black"), Tag.create(content: "friendly")]
-    )
-    Report.create(
-      user_id: 1,
-      pet_name: "ben",
-      animal_type: "dog",
-      lat: 12.34555555,
-      lng: 9.87655555,
-      report_type: "lost",
-      notes: "I lost my dog ben.",
-      img_url: "http://www.example.com/ben.jpg",
-      age: "baby",
-      breed: "german shephard",
-      sex: "male",
-      size: "small",
-      distance: 0.00,
-      tags: [Tag.create(content: "puppy")]
-    )
-#   ],
-# )
+User.create(
+  username: "testuser",
+  email: "testuser@example.com",
+  password_hash: "password",
+  zipcode: 12345,
+  wags: 0,
+)
 
-# Comment.create(
-#   user: User.first,
-#   report: Report.first,
-#   content: "blah blah blah",
-# )
+Report.create(
+  user: User.first,
+  animal_type: "dog",
+  lat: 12.34567890,
+  lng: 9.87654321,
+  report_type: "found",
+  notes: "I found this dog",
+  img_url: "http://www.example.com/lost_dog.jpg",
+  size: "small",
+  distance: 10.53,
+  tags: [Tag.create(name: "black"), Tag.create(name: "friendly")]
+)
+
+Report.create(
+  user: User.first,
+  pet_name: "ben",
+  animal_type: "dog",
+  lat: 12.34555555,
+  lng: 9.87655555,
+  report_type: "lost",
+  notes: "I lost my dog ben.",
+  img_url: "http://www.example.com/ben.jpg",
+  age: "baby",
+  breed: "german shephard",
+  sex: "male",
+  size: "small",
+  distance: 0.00,
+  tags: [Tag.create(name: "puppy")]
+)
+
+Comment.create(
+  user: User.first,
+  report: Report.first,
+  content: "blah blah blah",
+)
