beb328507e696d2f483d0ff5a2f93a7dcffdce12
Jeremy Gagon <jergagon1@gmail.com>
2015-08-27 18:03:25 -0700

    Delete unused code in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,7 @@
 class ApplicationController < ActionController::API
-  # helper_method :current_user
-  # before_action :current_user
-
-  #make a call to fuzzfriends-api to determine whether user is logged_in
-  #good proof of concept of apis making requests to each other
-  #but not secure
+  include ::ActionController::Serialization
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
 
   def find_user params
     @user ||= User.find_by(
@@ -12,20 +9,6 @@ class ApplicationController < ActionController::API
       password_hash: params[:password_hash],
     )
   end
-
-  # def current_user
-    # url = "http://localhost:3000/api/v1/logged_in?email=#{params[:email]}&password_hash=#{params[:password_hash]}"
-    # response = HTTParty.get(url)
-    # data = JSON.parse(response.body)
-  #   unless data["logged_in"]
-  #     render json: "invalid credentials", status: 401
-  #   else
-  #     @current_user ||= data
-  #   end
-  # end
-
-  before_filter :cors_preflight_check
-  after_filter :cors_set_access_control_headers
 
   def cors_set_access_control_headers
     headers['Access-Control-Request-Method'] = '*'
