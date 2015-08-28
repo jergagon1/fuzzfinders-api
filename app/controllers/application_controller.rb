c3e4647b476baf54c921c6df90ec22f1af3d8468
Jeremy Gagon <jergagon1@gmail.com>
2015-08-27 18:20:12 -0700

    Include code to use AMS - not implemented yet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  include ::ActionController::Serialization
+  # include ::ActionController::Serialization
   before_filter :cors_preflight_check
   after_filter :cors_set_access_control_headers
 
