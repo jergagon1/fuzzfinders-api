95d436436377cb82b7f6f9503fcfe2b21843fd35
Dmitry Zudochkin <dnd.pliz@gmail.com>
2015-12-07 15:02:47 +0300

    add coordinates fields to comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151201115124) do
+ActiveRecord::Schema.define(version: 20151207095159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 20151201115124) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "img_url"
+    t.float    "lat"
+    t.float    "lng"
   end
 
   create_table "images", force: :cascade do |t|
